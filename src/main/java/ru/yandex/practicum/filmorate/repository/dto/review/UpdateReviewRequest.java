dd4e26fd890e73388e8d6750773769054a6d4fe4
Katya <katyaveteer@yandex.ru>
2025-08-22 14:22:28 +0300

    Реализована фича "Отзывы"

--- src/main/java/ru/yandex/practicum/filmorate/repository/dto/review/UpdateReviewRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/dto/review/UpdateReviewRequest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.repository.dto.review;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
