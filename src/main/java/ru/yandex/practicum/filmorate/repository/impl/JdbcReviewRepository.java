2bb7d0eb58554f271f95175c2762fcc9d39e1140
YakovAvia <ralfffi@yandex.ru>
2025-08-28 13:46:34 +0300

    Исправил add-feed

--- src/main/java/ru/yandex/practicum/filmorate/repository/impl/JdbcReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/impl/JdbcReviewRepository.java
@@ -85,30 +85,30 @@ public class JdbcReviewRepository implements ReviewRepository {
         params.addValue("count", count);
 
         if (filmId == 0) {
-            sql = """      
-                    SELECT r.review_id, r.content, r.is_positive, r.user_id, r.film_id, r.useful,
-                           rr.user_id AS reaction_user_id, rr.is_like
-                    FROM (
-                        SELECT *
-                        FROM reviews
-                        ORDER BY useful DESC, review_id ASC
-                        LIMIT :count
-                    ) r
-                    LEFT JOIN review_reactions rr ON r.review_id = rr.review_id
-                    """;
+            sql = """
+            SELECT r.review_id, r.content, r.is_positive, r.user_id, r.film_id, r.useful,
+                   rr.user_id AS reaction_user_id, rr.is_like
+            FROM (
+                SELECT *
+                FROM reviews
+                ORDER BY useful DESC, review_id ASC
+                LIMIT :count
+            ) r
+            LEFT JOIN review_reactions rr ON r.review_id = rr.review_id
+            """;
         } else {
             sql = """
-                    SELECT r.review_id, r.content, r.is_positive, r.user_id, r.film_id, r.useful,
-                           rr.user_id AS reaction_user_id, rr.is_like
-                    FROM (
-                        SELECT *
-                        FROM reviews
-                        WHERE film_id = :film_id
-                        ORDER BY useful DESC, review_id ASC
-                        LIMIT :count
-                    ) r
-                    LEFT JOIN review_reactions rr ON r.review_id = rr.review_id
-                    """;
+            SELECT r.review_id, r.content, r.is_positive, r.user_id, r.film_id, r.useful,
+                   rr.user_id AS reaction_user_id, rr.is_like
+            FROM (
+                SELECT *
+                FROM reviews
+                WHERE film_id = :film_id
+                ORDER BY useful DESC, review_id ASC
+                LIMIT :count
+            ) r
+            LEFT JOIN review_reactions rr ON r.review_id = rr.review_id
+           """;
 
             params.addValue("film_id", filmId);
         }
